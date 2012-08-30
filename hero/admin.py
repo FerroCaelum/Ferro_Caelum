9e93c27a0b2e1d5b62080683937cae848c1e7e67
konrad <kcomrp@gmail.com>
2012-08-30 20:38:28 +0200

    Zmiana statystyk na decimal, dodanie opcji testowania wszystkich aktualnych klas w panelu admin, usuniecie zbednego pliku

---
+++ hero/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from hero.models import Hero, Atributs, Ba_proficiency, Ba_skills, Main_stats
+
+admin.site.register(Hero)
+admin.site.register(Atributs)
+admin.site.register(Ba_proficiency)
+admin.site.register(Ba_skills)
+admin.site.register(Main_stats)
