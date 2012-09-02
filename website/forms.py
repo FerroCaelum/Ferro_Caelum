57395eddc71dc7ac5423a0e7fdcadbc2ef74d03b
episage <tilosag@gmail.com>
2012-09-02 16:55:45 +0200

    lepszy commit

--- website/forms.py
+++ website/forms.py
@@ -2,10 +2,11 @@ from django import forms
 import django
 from django.forms.models import ModelForm
 from django.contrib.auth.models import User
-class SignUpForm(ModelForm):
+from hero import models
+class NewAccountForm(ModelForm):
     class Meta:
         model=User
-        fields=('login','password')
+#        fields=('username','password')
 #    login = forms.CharField()
 #    email = forms.CharField()
 #    password = forms.CharField(widget=forms.PasswordInput)
--- website/views.py
+++ website/views.py
@@ -1,53 +1,59 @@
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView
 from django.contrib.auth.models import User
-
-__author__ = 'episage'
+from django.forms.models import modelformset_factory
 from django.views.generic import TemplateView
-from forms import SignUpForm
+from forms import NewAccountForm
 from django.shortcuts import render
 from django.http import *
-
+from hero.models import Hero
+from django.template import RequestContext
 
 class Homepage(TemplateView):
     template_name = "homepage.html"
 
 
-class SignUpView(FormView):
+class AccountCreate(CreateView):
+    model=User
     template_name = "sign-up.html"
-    form_class = SignUpForm
-    success_url = "sign-up-success"
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        user = User.objects.create_user(
-            form.cleaned_data['login'],
-            form.cleaned_data['email'],
-            form.cleaned_data['password'])
-        user.save()
-        return HttpResponse('success')
-        return super(SignUpView, self).form_valid(form)
 
     def form_invalid(self, form):
         errors = [(k, force_unicode(v[0]) ) for k, v in form.errors.items()]
         return HttpResponse(errors)
 
+    def get_initial(self):
+        # Get the initial dictionary from the superclass method
+        initial = super(AccountCreate, self).get_initial()
+        # Copy the dictionary so we don't accidentally change a mutable dic
+        initial = initial.copy()
+        SignUpFormset = modelformset_factory(User, form=NewAccountForm)
+        formset=SignUpFormset()
 
-def sign_up(request):
-    if request.method == 'POST': # If the form has been submitted...
-        form = SignUpForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            user = User.objects.create_user(
-                form.cleaned_data['login'],
-                form.cleaned_data['email'],
-                form.cleaned_data['password'])
-            user.save()
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
-    else:
-        form = SignUpForm() # An unbound form
+        initial['formset'] = formset
+        # etc...
+        return initial
 
-    return render(request, 'sign-up.html', {
-        'form': form,
-        })
+#class SignUpFormView(FormView):
+#    template_name = "sign-up.html"
+#    form_class = NewAccountForm
+#    success_url = "sign-up-success"
+#
+#
+##    SignUpFormset = modelformset_factory(User, form=SignUpForm)
+##    formset=SignUpFormset()
+##    print formset
+##    test_val= ['kasia','asia',4,2,1]
+#
+#    def form_valid(self, form):
+#        # This method is called when valid form data has been POSTed.
+#        # It should return an HttpResponse.
+#        user = User.objects.create_user(
+#            form.cleaned_data['login'],
+#            form.cleaned_data['email'],
+#            form.cleaned_data['password'])
+#        user.save()
+##        return HttpResponse('success')
+##        return super(SignUpView, self).form_valid(form)
+#
+#    def form_invalid(self, form):
+#        errors = [(k, force_unicode(v[0]) ) for k, v in form.errors.items()]
+#        return HttpResponse(errors)
