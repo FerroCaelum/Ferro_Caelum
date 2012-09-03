c37b58e87d07bd1b72a70598ebf37c81fc4fc3f0
episage <tilosag@gmail.com>
2012-09-03 14:09:54 +0200

    podział na account

--- website/urls.py
+++ website/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import AccountCreate
 from views import Homepage
+from account import urls
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', Homepage.as_view()),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^sign-up/', AccountCreate.as_view()),
+    url(r'^account/', include(r'account.urls')),
 )
--- website/views.py
+++ website/views.py
@@ -1,40 +1,6 @@
-from django.forms.formsets import formset_factory
-from django.forms.models import modelformset_factory
-from django.views.generic.edit import FormView, CreateView
-from django.contrib.auth.models import User
 from django.views.generic import TemplateView
-from forms import SignUpForm
-from django.shortcuts import render
-from django.http import *
-from hero.models import *
 
 class Homepage(TemplateView):
     template_name = "homepage.html"
 
 
-class SignUpView(CreateView):
-    template_name = "sign-up.html"
-    form_class = SignUpForm
-    success_url = "sign-up-success"
-
-    def form_valid(self, form):
-        user = User.objects.create_user(
-            form.cleaned_data['username'],
-            form.cleaned_data['email'],
-            form.cleaned_data['password'])
-        user.save()
-        return HttpResponse('success')
-
-    #        return super(SignUpView, self).form_valid(form)
-
-    def form_invalid(self, form):
-        errors = [(k, force_unicode(v[0]) ) for k, v in form.errors.items()]
-        return HttpResponse(errors)
-
-    def get_context_data(self, **kwargs):
-        context = super(SignUpView, self).get_context_data(**kwargs)
-
-        SignUpFormSet = modelformset_factory(User, form=SignUpForm)
-        formset = SignUpFormSet(queryset=User.objects.none())
-        context['formset'] = formset.as_p()
-        return context
