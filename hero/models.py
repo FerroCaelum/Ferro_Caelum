7a521e2d09abefea26df49ef252b2892c8989ea8
konrad <kcomrp@gmail.com>
2012-09-05 15:05:30 +0200

    Poprawki na Hero app

--- hero/models.py
+++ hero/models.py
@@ -2,41 +2,39 @@
 
 from django.db import models
 
-'''
-Wszystkie statystyki odnoszące się do bohatera powinne być w 1 tabeli.
-
-Większość wartości defaultowych nie będzie używanych z uwagi na fakt, że statsy każdego bohatera będą przypisywane przy
-rejestracji w zależności od wyboru linii krwi, czy innych "rzeczy".
-
-Poprawiłem angielskie nazwy.
-'''
 
 class Hero(models.Model):
-    name = models.CharField(
-        max_length=50) # http://stackoverflow.com/questions/20958/list-of-standard-lengths-for-database-fields
-    energy = models.PositiveIntegerField(default=0.0)
+    name = models.CharField(max_length=60)
+    lvl = models.PositiveIntegerField(default=1)
+    experience = models.PositiveIntegerField(default=0)
+    energy = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=20.0)
+    gold = models.DecimalField(max_digits=20, decimal_places=2, default=0.0) #stan konta
     #atrybuty
-    power = models.PositiveIntegerField(default=1.0) #moc
-    resistance = models.PositiveIntegerField(default=1.0) #wytrzymałość
-    dexterity = models.PositiveIntegerField(default=1.0) #zręczność
-    perception = models.PositiveIntegerField(default=1.0) #percepcja
-    intelligence = models.PositiveIntegerField(default=1.0) #inteligencja
-    web = models.PositiveIntegerField(default=1.0) #sieć
-    artifice = models.PositiveIntegerField(default=1.0) #spryt
+    power = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #moc
+    resistance = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #wytrzymałość
+    dexterity = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #zręczność
+    perception = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #percepcja
+    intelligence = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #inteligencja
+    web = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #sieć
+    artifice = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #spryt
     #statystyki główne
-    hp = models.PositiveIntegerField(default=10) #punkty życia
+    hp = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=10.0) #punkty życia
     ap = models.PositiveIntegerField(default=100) #punkty akcji
-    speed = models.PositiveIntegerField(default=5) #prędkość http://pl.wikipedia.org/wiki/Kilometr_na_godzin%C4%99
-    #statystyki bojowe
-    camouflage = models.PositiveIntegerField(default=0.0) #kamuflaż
-    detection = models.PositiveIntegerField(default=0.0) #detekcja
-    #umiejętności walki
-    melee_attack = models.PositiveIntegerField(default=0.0) #atak wręcz
-    range_attack = models.PositiveIntegerField(default=0.0) #atak dystansowy
-    programming = models.PositiveIntegerField(default=0.0) #programowanie
-    web_use = models.PositiveIntegerField(default=0.0) #używanie sieci
-    antivirus_use = models.PositiveIntegerField(default=0.0) #obrona antywirusowa
-    dodge = models.PositiveIntegerField(default=0.0) #uniki
-    camouflage_use = models.PositiveIntegerField(default=0.0) #kamuflowanie się
-    detection_use = models.PositiveIntegerField(default=0.0) #wykrywanie
-    quick_move = models.PositiveIntegerField(default=1.0) #szybkie poruszanie się
+    speed = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=1.0) #prędkość
+    #umiejętności
+    detection = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #detekcja
+    hide = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #kamuflaż
+    trade = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #handel
+    #wyćwiczenie
+    #>Bojowe
+    melee_attack = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak wręcz
+    range_attack = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak dystansowy
+    programming = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #programowanie
+    web_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #używanie sieci
+    antivirus_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #obrona antywirusowa
+    dodge = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #uniki
+    quick_move = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #szybkie poruszanie się
+    #>Umiejętności
+    detection_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #wykrywanie
+    hide_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #ukrywanie się
+    trade_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #handlowanie
