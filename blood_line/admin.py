1c630e9086f8c4d0af2839135920da75c3bfa789
konrad <kcomrp@gmail.com>
2012-09-07 00:52:16 +0200

    Poprawki modelu Hero + linie krwi + profesje + specialne zdolnosci(jeszcze w budowie)

---
+++ blood_line/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from blood_line.models import Blood_line
+
+admin.site.register(Blood_line)
--- hero/models.py
+++ hero/models.py
@@ -1,40 +1,89 @@
 # coding: utf-8
 
 from django.db import models
+from django.core.validators import MinValueValidator
 
+class BloodLine(models.Model):
+    name = models.CharField(max_length=50) #blood line name
+    power = models.PositiveIntegerField(default=8) #moc
+    resistance = models.PositiveIntegerField(default=8) #wytrzymałość
+    dexterity = models.PositiveIntegerField(default=8) #zręczność
+    perception = models.PositiveIntegerField(default=8) #percepcja
+    intelligence = models.PositiveIntegerField(default=8) #inteligencja
+    web = models.PositiveIntegerField(default=8) #sieć
+    artifice = models.PositiveIntegerField(default=8) #spryt
+    #podstawowe
+    hp = models.PositiveIntegerField(default=10) #punkty życia
+    ap = models.PositiveIntegerField(default=100) #punkty akcji
+    speed = models.PositiveIntegerField(default=100) #prędkość
+    #umiejętności
+    detection = models.PositiveIntegerField(default=0) #detekcja
+    hide = models.PositiveIntegerField(default=0) #kamuflaż
+    trade = models.PositiveIntegerField(default=0) #handel
+    
+class Profession(models.Model):
+    power_cost = models.PositiveIntegerField(default=30) #moc
+    resistance_cost = models.PositiveIntegerField(default=30) #wytrzymałość
+    dexterity_cost = models.PositiveIntegerField(default=30) #zręczność
+    perception_cost = models.PositiveIntegerField(default=30) #percepcja
+    intelligence_cost = models.PositiveIntegerField(default=30) #inteligencja
+    web_cost = models.PositiveIntegerField(default=30) #sieć
+    artifice_cost = models.PositiveIntegerField(default=30) #spryt
+    #umiejętności
+    detection_cost = models.PositiveIntegerField(default=10) #detekcja
+    hide_cost = models.PositiveIntegerField(default=10) #kamuflaż
+    trade_cost = models.PositiveIntegerField(default=10) #handel
 
 class Hero(models.Model):
     name = models.CharField(max_length=60)
     lvl = models.PositiveIntegerField(default=1)
+    lvl_points = models.PositiveIntegerField(default=100)
+    blood_line = models.ForeignKey(BloodLine)   
+    profession = models.ForeignKey(Profession)
     experience = models.PositiveIntegerField(default=0)
-    energy = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=20.0)
+    energy = models.PositiveIntegerField(default=20)
     gold = models.DecimalField(max_digits=20, decimal_places=2, default=0.0) #stan konta
     #atrybuty
-    power = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #moc
-    resistance = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #wytrzymałość
-    dexterity = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #zręczność
-    perception = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #percepcja
-    intelligence = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #inteligencja
-    web = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #sieć
-    artifice = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #spryt
+    power = models.PositiveIntegerField(default=1) #moc
+    resistance = models.PositiveIntegerField(default=1) #wytrzymałość
+    dexterity = models.PositiveIntegerField(default=1) #zręczność
+    perception = models.PositiveIntegerField(default=1) #percepcja
+    intelligence = models.PositiveIntegerField(default=1) #inteligencja
+    web = models.PositiveIntegerField(default=1) #sieć
+    artifice = models.PositiveIntegerField(default=1) #spryt
     #statystyki główne
-    hp = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=10.0) #punkty życia
+    hp = models.PositiveIntegerField(default=10) #punkty życia
     ap = models.PositiveIntegerField(default=100) #punkty akcji
-    speed = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=1.0) #prędkość
+    speed = models.PositiveIntegerField(default=100) #prędkość
     #umiejętności
-    detection = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #detekcja
-    hide = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #kamuflaż
-    trade = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #handel
+    detection = models.PositiveIntegerField(default=0) #detekcja
+    hide = models.PositiveIntegerField(default=0) #kamuflaż
+    trade = models.PositiveIntegerField(default=0) #handel
     #wyćwiczenie
     #>Bojowe
-    melee_attack = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak wręcz
-    range_attack = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak dystansowy
-    programming = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #programowanie
-    web_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #używanie sieci
-    antivirus_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #obrona antywirusowa
-    dodge = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #uniki
-    quick_move = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(1.0)], default=1.0) #szybkie poruszanie się
+    melee_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak wręcz
+    range_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak dystansowy
+    programming = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #programowanie
+    web_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #używanie sieci
+    antivirus_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #obrona antywirusowa
+    dodge = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #uniki
+    quick_move = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #szybkie poruszanie się
     #>Umiejętności
-    detection_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #wykrywanie
-    hide_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #ukrywanie się
-    trade_use = models.DecimalField(max_digits=15, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #handlowanie
+    detection_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #wykrywanie
+    hide_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #ukrywanie się
+    trade_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #handlowanie 
+
+class Ability(models.Model):
+    TYPE_CHOICES = (
+        ('p', 'Pasywna, modyfikuje jednokrotnie'),
+        ('p-a', 'Pasywna, stale modyfikuje'),
+        ('a', 'Aktywowana'),
+    )
+    type = models.CharField(max_length=3, choices=TYPE_CHOICES, default='p')
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    hero = models.ManyToManyField(Hero)
+       
+class BloodLineAbility(models.Model):
+    blood_line = models.ForeignKey(BloodLine)
+    ability = models.OneToOneField(Ability)
