1fb14f1a8ab55b9d2801242d902c867da238363f
konrad <kcomrp@gmail.com>
2012-09-23 23:11:32 +0200

    backup

--- hero/models.py
+++ hero/models.py
@@ -5,47 +5,73 @@ from django.core.validators import MinValueValidator
 from blood_line.models import BloodLine
 from profession.models import Profession
 from talent.models import Talent
+from effect.models import *
     
 
 class Hero(models.Model):
-    name = models.CharField(max_length=60)
-    lvl = models.PositiveIntegerField(default=1)
-    lvl_points = models.PositiveIntegerField(default=100)
-    blood_line = models.ForeignKey(BloodLine)   
-    profession = models.ForeignKey(Profession)
-    experience = models.PositiveIntegerField(default=0)
-    energy = models.PositiveIntegerField(default=20)
-    energy_regeneration = models.PositiveIntegerField(default=20)
-    gold = models.DecimalField(max_digits=20, decimal_places=2, default=0.0) #stan konta
+#    name = models.CharField(max_length=60)
+#    lvl = models.PositiveIntegerField(default=1)
+#    lvl_points = models.PositiveIntegerField(default=100)
+#    blood_line = models.ForeignKey(BloodLine)   
+#    profession = models.ForeignKey(Profession)
+#    experience = models.PositiveIntegerField(default=0)
+#    energy = models.PositiveIntegerField(default=20)
+#    energy_regeneration = models.PositiveIntegerField(default=20)
+#    gold = models.DecimalField(max_digits=20, decimal_places=2, default=0.0) #stan konta
     talent = models.ManyToManyField(Talent)
-    #atrybuty
+#    #atrybuty
     power = models.PositiveIntegerField(default=1) #moc
-    resistance = models.PositiveIntegerField(default=1) #wytrzymałość
-    dexterity = models.PositiveIntegerField(default=1) #sprawność
-    perception = models.PositiveIntegerField(default=1) #percepcja
-    intelligence = models.PositiveIntegerField(default=1) #inteligencja
-    web = models.PositiveIntegerField(default=1) #sieć
-    artifice = models.PositiveIntegerField(default=1) #spryt
-    #statystyki główne
-    hp = models.PositiveIntegerField(default=10) #punkty życia
-    ap = models.PositiveIntegerField(default=100) #punkty akcji
-    speed = models.PositiveIntegerField(default=100) #prędkość
-    #umiejętności
-    detection = models.PositiveIntegerField(default=0) #detekcja
-    hide = models.PositiveIntegerField(default=0) #kamuflaż
-    trade = models.PositiveIntegerField(default=0) #handel
-    #wyćwiczenie (doświadczenie)
-    #>Bojowe
-    melee_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak wręcz
-    range_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak dystansowy
-    programming = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #programowanie
-    web_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #używanie sieci
-    antivirus_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #obrona antywirusowa
-    dodge = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #uniki
-    quick_move = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #szybkie poruszanie się
-    #>Umiejętności
-    detection_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #wykrywanie
-    hide_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #ukrywanie się
-    trade_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #handlowanie 
+#    resistance = models.PositiveIntegerField(default=1) #wytrzymałość
+#    dexterity = models.PositiveIntegerField(default=1) #sprawność
+#    perception = models.PositiveIntegerField(default=1) #percepcja
+#    intelligence = models.PositiveIntegerField(default=1) #inteligencja
+#    web = models.PositiveIntegerField(default=1) #sieć
+#    artifice = models.PositiveIntegerField(default=1) #spryt
+#    #statystyki główne
+#    hp = models.PositiveIntegerField(default=10) #punkty życia
+#    ap = models.PositiveIntegerField(default=100) #punkty akcji
+#    speed = models.PositiveIntegerField(default=100) #prędkość
+#    #umiejętności
+#    detection = models.PositiveIntegerField(default=0) #detekcja
+#    hide = models.PositiveIntegerField(default=0) #kamuflaż
+#    trade = models.PositiveIntegerField(default=0) #handel
+#    #wyćwiczenie (doświadczenie)
+#    #>Bojowe
+#    melee_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak wręcz
+#    range_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak dystansowy
+#    programming = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #programowanie
+#    web_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #używanie sieci
+#    antivirus_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #obrona antywirusowa
+#    dodge = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #uniki
+#    quick_move = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #szybkie poruszanie się
+#    #>Umiejętności
+#    detection_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #wykrywanie
+#    hide_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #ukrywanie się
+#    trade_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #handlowanie 
+    def get_hero_effects(self):
+        talents = Talent.objects.filter(hero__pk=self.pk)
+        acumulator=[]
+        for t in talents:
+            acumulator += EffectOfTalent.objects.filter(talent__pk=t.pk)
+        return acumulator
+    def get_main_stats_effect(self):
+        return self.get_hero_effects().filter(where_works=1)
+    def get_updated_power(self):
+        effects = self.get_main_stats_effect().filter(variable=1)
+        es = effects.filter(percent=False)
+        sum = 0
+        for effect in es:
+            sum+=effect.value     
+        em = effects.filter(percent=True)
+        multi = 100
+        for effect in em:
+            multi+=effect.value
+        return 0.01*multi*(self.power+sum)
+    def print_all_effects(self):
+        effect=self.get_hero_effects()
+        for e in effect: 
+            print e.get_description()
+    def get_updated_power(self):
+        return 
     def __unicode__(self):
         return self.name
--- talent/models.py
+++ talent/models.py
@@ -12,23 +12,23 @@ class Talent(models.Model):
     talent_requirements = models.ManyToManyField('self', through='TalentRequierment',
                                                     symmetrical=False, related_name='requires')
     blood_line_requirement = models.ForeignKey(BloodLine, null=True)
-    def add_required_ability(self, ability):
-        ability_requierment, created = TalentRequierment.objects.get_or_create(
+    def add_required_talent(self, talent):
+        talent_requierment, created = TalentRequierment.objects.get_or_create(
             require=self,
-            required=ability)
-        return ability_requierment
-    def remove_required_ability(self, ability):
+            required=talent)
+        return talent_requierment
+    def remove_required_talent(self, talent):
         TalentRequierment.objects.filter(
              require=self,
-             required=ability).delete()
+             required=talent).delete()
         return     
     def get_abilities_requierments(self):
         return self.talent_requirements.filter(
              required__require=self)
     def get_effects(self):
-        return EffectOfTalent.objects.filter(ability__pk=self.pk)
+        return EffectOfTalent.objects.filter(talent__pk=self.pk)
     def get_stats_requierments(self):
-        return StatsRequierment.objects.filter(ability__pk=self.pk)
+        return StatsRequierment.objects.filter(talent__pk=self.pk)
     def get_talent_requirements_description(self):
          requiraments = self.get_talent_requirements()
          descriptions = self.name + ' wymaga: '
@@ -44,7 +44,7 @@ class Talent(models.Model):
              if r!=requiraments[requiraments.count()-1]:  descriptions += ", "
          return descriptions
     def get_effects(self):
-        return EffectOfTalent.objects.filter(ability__pk=self.pk)
+        return EffectOfTalent.objects.filter(talent__pk=self.pk)
     def get_effects_description(self):
          effects = self.get_effects()
          descriptions = self.name + ' zmienia: '
@@ -65,7 +65,7 @@ class StatsRequirement(models.Model):
     variable = models.PositiveSmallIntegerField();
     def get_variable_string(self, where):
         return stats_dictionary.get_stats_name(self.variable, where)
-    def get_description(self, ability):
+    def get_description(self, talent):
         des = self.get_variable_string(1) + ': '                           
         return des + str(self.value)       
     def __unicode__(self):
