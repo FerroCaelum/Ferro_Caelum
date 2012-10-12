ddbcdb51d7b8aa222726b47e9374f0f926996d33
konrad <kcomrp@gmail.com>
2012-10-12 23:03:21 +0200

    zapis przerwy w pracy

--- talent/models.py
+++ talent/models.py
@@ -4,6 +4,19 @@ from django.db import models
 from blood_line.models import BloodLine
 from effect.models import Effect
 import general.stats_dictionary
+   
+class StatsRequirement(models.Model):
+    """Wymagania w statystykach bohatera"""
+    value = models.PositiveSmallIntegerField();
+    variable = models.PositiveSmallIntegerField();
+    def get_variable_string(self, where):
+        return stats_dictionary.get_stats_name(self.variable, where)
+    def get_description(self, talent):
+        des = self.get_variable_string(1) + ': '                           
+        return des + str(self.value)       
+    def __unicode__(self):
+        return "Wymaganie statystyk " + str(self.id)
+
 
 class Talent(models.Model):
     """Przy awnsie można go wybrać przy awansie, jeśli posiada się wymagane talenty i ma wystarczające wartości statystyk.
@@ -12,39 +25,18 @@ class Talent(models.Model):
     effects = models.ManyToManyField(Effect)
     additional_description = models.CharField(max_length=200, blank=True)
     stats_requirements = models.ManyToManyField(StatsRequirement, blank=True)
-    talent_requirements = models.ManyToManyField('self', through='TalentRequierment',
-                                                    symmetrical=False, related_name='requires')
+    talents_required = models.ManyToManyField('self', symmetrical=False)
     blood_line_requirement = models.ForeignKey(BloodLine, null=True)  
-   
-    def add_required_talent(self, talent):
-        """Dodaje do wymagań talent, który trzeba wziąć by wykupić talent, z którego jest wywołana metoda"""
-        talent_requierment, created = TalentRequierment.objects.get_or_create(
-            require=self,
-            required=talent)
-        return talent_requierment
-    
-    def remove_required_talent(self, talent):
-        """Usuwa wymagany talent, konieczny do wykupienia talentu, z którego jest wywołana ta metoda"""
-        TalentRequierment.objects.filter(
-             require=self,
-             required=talent).delete()
-        return     
-    
-    def get_talents_requierments(self):
-        """Zwraca listę talentów, wymaganych do wykupienia talentu, z którgo jest wywoływana ta metoda"""
-        return self.talent_requirements.filter(
-             required__require=self)
-        
     def get_effects(self):
         """Zwraca listę efektów przypisanych do"""
         pass
     
     def get_stats_requierments(self):
-        """Zwraca listę wymagań w statystykach, potrzebnych do wykupienia talentu, z którego jest woływana ta metoda"""
+        """Zwraca listę wymagań w statystykach, koniecznych do wykupienia talentu, z którego jest woływana ta metoda"""
         return StatsRequierment.objects.filter(talent__pk=self.pk)
     
     def get_talent_requirements_description(self):
-        """Zwraca słowny opis wymaganych talentów, potrzebnych do wykupienia talentu, z którgo jest wywoływana ta metoda"""
+        """Zwraca słowny opis wymaganych talentów, koniecznych do wykupienia talentu, z którgo jest wywoływana ta metoda"""
         requiraments = self.get_talent_requirements()
         descriptions = self.name + ' wymaga: '
         for a in requiraments:
@@ -53,7 +45,7 @@ class Talent(models.Model):
         return descriptions      
      
     def get_stats_requirements_description(self):
-         """Zwraca słowny opis wymaganych statystyk, potrzebnych do wykupienia talentu, z którgo jest wywoływana ta metoda"""
+         """Zwraca słowny opis wymaganych statystyk, koniecznych do wykupienia talentu, z którgo jest wywoływana ta metoda"""
          requiraments = self.get_stats_requierments()
          descriptions = self.name + ' wymaga '
          for r in requiraments:
@@ -72,22 +64,3 @@ class Talent(models.Model):
      
     def __unicode__(self):
         return self.name
-   
-   
-class TalentRequierment(models.Model):
-    """Klasa potrzebna do budowy drzewa rozwoju talentów"""
-    require = models.ForeignKey(Talent, related_name='require')
-    required = models.ForeignKey(Talent, related_name='required')
-    
-class StatsRequirement(models.Model):
-    """Wymagania w statystykach bohatera"""
-    talent = models.ForeignKey(Talent)
-    value = models.PositiveSmallIntegerField();
-    variable = models.PositiveSmallIntegerField();
-    def get_variable_string(self, where):
-        return stats_dictionary.get_stats_name(self.variable, where)
-    def get_description(self, talent):
-        des = self.get_variable_string(1) + ': '                           
-        return des + str(self.value)       
-    def __unicode__(self):
-        return "Wymaganie statystyk " + str(self.id)
--- talent/tests.py
+++ talent/tests.py
@@ -5,12 +5,22 @@ from talent.models import Talent
 from effect.models import Effect
 
 
-class SimpleTest(TestCase):
+class TalentsEffectsTest(TestCase):
     def setUp(self):
         self.talent1 = Talent(name="Overpower III")
+        self.talent1.save()
+        self.talent1.effects.create(value=5, variable=4, percent=False, where_works=1)
     
     def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
         self.assertEqual(1 + 1, 2)
+        
+class TalentsTreeTest(TestCase):
+    def setUp(self):
+        self.talent1 = Talent(name="Overpower III")
+        self.talent1.save()
+        self.talent1.talents_required.create(name="Overpower II")
+        
+    def test_if_one_side_relation(self):
+        self.assertFalse(
+                self.talent1.talents_required.all() ==
+                self.talent1.talents_required.all()[0].talents_required.all())
