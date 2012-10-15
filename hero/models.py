190b236dcbdc86364f94d74aee815a839d958d1c
episage <tilosag@gmail.com>
2012-10-15 02:36:14 +0100

    Owner.give(item, new_owner, count)

--- hero/models.py
+++ hero/models.py
@@ -2,24 +2,54 @@
 
 from django.db import models
 from django.core.validators import MinValueValidator
+from armory.models import ItemInstance, Item
 from blood_line.models import BloodLine
 from profession.models import Profession
 from talent.models import Talent
-  
-class Owner(models.Model):
-    max_load = models.DecimalField(max_digits=10, decimal_places=2,
-        default=80) #+funkcja agregująca obecny ładunek (nie umiem)
-    name = models.CharField(
-        max_length=50
-    ) # http://stackoverflow.com/questions/20958/list-of-standard-lengths-for-database-fields
+
+class Owner:
+    name = models.CharField(max_length=50)
+
+    max_load = models.DecimalField(max_digits=10, decimal_places=2, default=80)
+    load = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def give(self, item, new_owner, count):
+        """
+        Przekazuje item nowemu właścicielowi.
+
+        :param new_owner: komu przekazać item'y
+        :type new_owner: hero.Owner
+        :param count: ilość
+        :type count: int
+        :returns: przekazane item'y
+        :rtype: Items
+        """
+
+        assert item is Item
+        assert new_owner is Owner
+        assert count is int
+
+        itemInstances = ItemInstance.objects.filter(owner=self, item=item)
+        itemInstancesCount = itemInstances.count()
+        if itemInstancesCount <= 0: raise u'%s does not have any of %s.' % self.name, item.name
+        if itemInstancesCount > 0: raise u'Server error! Item=%s Owner=%s.' % item.name, self.name
+        itemInstance = itemInstances[0]
+        ownedCount = itemInstance.count
+        if ownedCount < count: raise u'%s cannot give more than they have of %s.' % self.name, item.name
+        itemInstance.count -= count
+        itemInstance.item.spawn(count, new_owner)
+
+    class Meta:
+        abstract = True
 
     def __unicode__(self):
-        return u'%s' % self.name  
+        return u'%s' % self.name
 
-class Hero(Owner):
+
+class Hero(Owner, models.Model):
     lvl = models.PositiveIntegerField(default=1)
     lvl_points = models.PositiveIntegerField(default=100)
-    blood_line = models.ForeignKey(BloodLine)   
+    blood_line = models.ForeignKey(BloodLine)
     profession = models.ForeignKey(Profession)
     experience = models.PositiveIntegerField(default=0)
     energy = models.PositiveIntegerField(default=20)
@@ -44,16 +74,31 @@ class Hero(Owner):
     trade = models.PositiveIntegerField(default=0) #handel
     #wyćwiczenie (doświadczenie)
     #>Bojowe
-    melee_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak wręcz
-    range_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #atak dystansowy
-    programming = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #programowanie
-    web_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #używanie sieci
-    antivirus_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #obrona antywirusowa
-    dodge = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #uniki
-    quick_move = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #szybkie poruszanie się
+    melee_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #atak wręcz
+    range_attack = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #atak dystansowy
+    programming = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #programowanie
+    web_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #używanie sieci
+    antivirus_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #obrona antywirusowa
+    dodge = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #uniki
+    quick_move = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #szybkie poruszanie się
     #>Umiejętności
-    detection_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #wykrywanie
-    hide_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #ukrywanie się
-    trade_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)], default=0.0) #handlowanie 
+    detection_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #wykrywanie
+    hide_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #ukrywanie się
+    trade_use = models.DecimalField(max_digits=10, decimal_places=4, validators=[MinValueValidator(0.0)],
+        default=0.0) #handlowanie
+
+    def equip(self, itemInstance):
+        pass
+
+
     def __unicode__(self):
         return self.name
