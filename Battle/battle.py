8344364553316360bb1cf271efc785cf189d9e2d
Mateusz Gizycki <mat20041@gmail.com>
2012-10-22 22:08:10 +0200

    Initial Commit :)

---
+++ Battle/battle.py
@@ -0,0 +1,12 @@
+__author__ = 'Mati'
+
+
+
+class Battle :
+    def __init__(self,hero1,hero2):
+        self.__hero1 = hero1
+        self.__hero2 = hero2
+        init_heroes
+
+    def init_heroes(self):
+        pass
